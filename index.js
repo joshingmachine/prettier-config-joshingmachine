7bde55dd324af832d277c884e825b256003bc2b5
joshingmachine <josh@joshingmachine.com>
2018-03-06 18:53:00 -0500

    Added Prettier configuration options

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: 'avoid',
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    requirePragma: false,
+    insertPragma: false,
+}
